628e33ca06284c7509ec66fc2b447230b1826653
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-02-03 23:52:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,7 +31,7 @@ class Dog
         SQL
         DB[:conn].execute(sql,self.name, self.breed) 
         
-     #  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
        self 
       end 
       
@@ -49,4 +49,28 @@ class Dog
     }
     self.new(hash)
   end
+  
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs 
+    WHERE dogs.id = ?
+    SQL
+    
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+ end
+ 
+ def self.find_or_create_by(name:, breed:)
+   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+   if !dog.empty?
+     dog_data = dog[0]
+     dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+   else
+     dog = self.create(name: name, breed: breed)
+   end
+   dog 
+ end 
+end
+end 
 end 
