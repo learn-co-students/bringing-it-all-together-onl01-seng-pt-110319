3ec2164d0437bfd9a73f9919d0d427d5ae439330
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-02-04 22:01:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Dog 
   
   attr_accessor :name, :breed, :id
@@ -61,16 +62,25 @@ class Dog
     end.first
  end
  
+
  def self.find_or_create_by(name:, breed:)
-   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?")
-    if !dog.empty?
-      dog_data = dog[0]
-     dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
-   else
-     dog = self.create(name: name, breed: breed)
-   end
-   dog 
- end 
+   
+   
+   sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL
+
+
+      dog = DB[:conn].execute(sql, name, breed).first
+
+      if dog
+        new_dog = self.new_from_db(dog)
+      else
+        new_dog = self.create({:name => name, :breed => breed})
+      end
+      new_dog
+  end
  
  def self.find_by_name(name)
    sql = <<-SQL 
