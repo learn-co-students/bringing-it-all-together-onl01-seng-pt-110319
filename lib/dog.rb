ddb37cc1d7bf59328d799d32e8516deb5d10cc47
rhaynes3k <rhaynes3k@gmail.com>
2020-02-10 00:19:34 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,49 @@
+class Dog
+  attr_accessor :id, :name, :breed
+  def initialize(id: id=nil, name: name, breed: breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = "INSERT INTO dogs(name, breed) VALUES(?, ?)"
+      new_row = DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      new_row.map do |row|
+        self.new(row)
+      end.first
+    end
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed)
+  end
+
+
+
+end
